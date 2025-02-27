50caab80ff582928806eddc7eadeee4b6577d8ee
Artiyom <artiyom.moskalew.2@gmail.com>
2025-02-27 18:23:38 +0100

    return ai generated image on imgage endpoint

--- api/image.py
+++ api/image.py
@@ -1,7 +1,24 @@
 from flask import Blueprint
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 image_api = Blueprint('image_api', __name__)
 
 @image_api.route("/api/image")
 def accountList():
-    return "image"
+    client = OpenAI(api_key = OPENAI_API_KEY)
+
+    response = client.images.generate(
+        model="dall-e-3",
+        prompt="story about a princess being chased by a dragon",
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+
+    return response.data[0].url
