d8c8c2e0bb95efde00a8ecca52fe4aa9b2e8aefa
Artiyom <artiyom.moskalew.2@gmail.com>
2025-03-04 19:41:44 +0100

    add logo and style the header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header";
 
 const layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <main>
+    <main className="max-w-5xl mx-auto ">
       <Header />
       {children}
     </main>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import aiBookLogo from "../assets/aiBook.svg";
+import Image from "next/image";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex justify-between">
+      <div>
+        <Image src={aiBookLogo} alt="logo" height={40} className="m-3" />
+      </div>
+      <div className="flex items-center mx-3">Login</div>
+    </div>
+  );
 };
 
 export default Header;
