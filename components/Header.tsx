7990d3ad8197ff527159432ef180301c1fbc065a
Artiyom <artiyom.moskalew.2@gmail.com>
2025-03-04 21:20:52 +0100

    link login button and home button

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,24 @@
 import React from "react";
 import aiBookLogo from "../assets/aiBook.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="fixed flex w-full m-w-inherit">
       <div className="grow-0 p-3">
-        <Image src={aiBookLogo} alt="logo" height={40} />
+        <Link href={"/"}>
+          <Image src={aiBookLogo} alt="logo" height={40} />
+        </Link>
       </div>
       <div className="flex items-center grow-100000">Searchbar</div>
-      <div className="flex items-center mx-3 grow-0">Login</div>
+      <div className="flex items-center mx-3 grow-0">
+        <Link href={"/login"}>
+          <button className="cursor-pointer bg-black text-white p-3">
+            Login
+          </button>
+        </Link>
+      </div>
     </header>
   );
 };
