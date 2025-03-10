ace95c73296b71c0979968334b80a4ceb83db6e5
Artiyom <artiyom.moskalew.2@gmail.com>
2025-03-10 15:00:47 +0100

    reset comment input on submission

--- components/CommentInput.tsx
+++ components/CommentInput.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "./ui/input";
 import Image from "next/image";
 import { addCommentToPost } from "@/app/actions/commentAction";
@@ -22,6 +22,7 @@ const CommentInput = ({
     if (content !== "") {
       await addCommentToPost({ postId, content, authorId });
     }
+    e.target.reset();
   };
 
   return (
